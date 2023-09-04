8b0eaca830fd64fa7f31041f0c6e6551f870acf6
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-09-04 21:34:45 +0700

    Finish Acara 11- add Intent finish

--- app/src/main/java/com/polije/tif/wma_minggu3_3/MainActivity.java
+++ app/src/main/java/com/polije/tif/wma_minggu3_3/MainActivity.java
@@ -2,13 +2,20 @@ package com.polije.tif.wma_minggu3_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.materialdesign);
+    }
+
+    public void btnMaterialDesign(View view) {
+        Intent pindahMaterialDesign = new Intent(MainActivity.this, TableLayout.class);
+        startActivity(pindahMaterialDesign);
     }
 }
